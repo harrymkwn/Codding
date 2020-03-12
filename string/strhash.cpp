cbe29b7a868d376e9e2a3e1b47ecf8eaaeabe96e
Herat Makwana <heratmakwana19@gmail.com>
2020-03-12 21:55:49 +0530

    string hash value

---
+++ string/strhash.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define F(a,b,i) for(int i=a;i<b;i++)
+#define f(a,b,i) for(int i=a;i>=b;i--)
+#define m 1000000007
+ll shash(string const& str){
+    ll p = 31;
+    ll hvalue=0;
+    ll mul=1;
+    F(0,str.size(),i){
+        hvalue= (hvalue + (str[i]*mul)%m)%m;
+        mul = (mul * p)%m;
+    }
+
+    return hvalue;
+}
+
+
+int main(){
+    string abc;
+    cin>>abc;
+    cout<<shash(abc);
+}
