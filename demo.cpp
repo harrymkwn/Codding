ff579f26ca245ed1d003851b084b59ab8ecd000d
Herat Makwana <heratmakwana19@gmail.com>
2020-03-20 22:38:12 +0530

    vector print-template added

--- demo.cpp
+++ demo.cpp
@@ -5,13 +5,25 @@ using namespace std;
 #define f(a, b, i) for (int i = a; i >= b; i--)
 #define m 1000000007
 
+template<typename T>
+void vp(T a){
+    for(int i=0;i<a.size();i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<"\n";
+}
 int main(){
 
 
-    vector<int> arr = {1,2,3,3,4,5};
-    auto pos = std::adjacent_find(std::begin(arr), std::end(arr));
-    if (pos != std::end(arr)) {
-      cout<<"yaay there is present ";
-    }
+    // vector<int> arr = {1,2,3,3,4,5};
+    // auto pos = std::adjacent_find(std::begin(arr), std::end(arr));
+    // if (pos != std::end(arr)) {
+    //   cout<<"yaay there is present ";
+    // }
+
+    ///////////////////////////////////////////////////////////////////////////////////
+
+    vector<int> arr= {1,2,3,4,5};
+    vp(arr);
   return 0;
 }
