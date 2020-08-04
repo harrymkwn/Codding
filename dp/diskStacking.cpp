db4611b7e835d7bbdb4d52fb4b4faa1813bbdf22
harrymkwn <heratmakwana19@gmail.com>
2020-08-04 14:06:27 +0530

    disk stacking completed

--- dp/diskStacking.cpp
+++ dp/diskStacking.cpp
@@ -8,18 +8,27 @@ bool check(vector<int> a, vector<int> b)
     return false;
 }
 
+bool availDimensions(vector<int> o,vector<int> c){
+    return (c[0] > o[0] && c[1] > o[1] && c[2] > o[2]);
+}
+
 vector<vector<int>> diskStacking(vector<vector<int>> disks)
-{
+{   
+    sort(disks.begin(),disks.end(),[](vector<int> a,vector<int> b){return a[2]<b[2];});
+    for (int i = 0; i < disks.size(); i++)
+    {
+        cout << disks[i][0] << " " << disks[i][1] << " " << disks[i][2] << "\n";
+    }
     vector<vector<int>> dp(disks.size(), {0, INT_MIN});
     vector<vector<int>> ret;
     int maxInd = 0;
+    dp[0][0] = disks[0][2];
     for (int i = 1; i < disks.size(); i++)
     {
         dp[i][0] = disks[i][2];
-        dp[i][1] = i;
         for (int j = 0; j < i; j++)
         {
-            if (disks[i][0] > disks[j][0] && disks[i][1] > disks[j][1] && disks[i][2] > disks[j][2])
+            if (availDimensions(disks[j],disks[i]))
             {
                 if (dp[i][0] <= dp[j][0] + disks[i][2])
                 {
@@ -27,19 +36,16 @@ vector<vector<int>> diskStacking(vector<vector<int>> disks)
                     dp[i][1] = j;
                 }
             }
-            if (dp[i][0] < disks[j][2]){
-                dp[i][0] = disks[j][2];
-                dp[i][1] = j;
-            }
-            if (dp[maxInd][0] <= dp[i][0])
-                maxInd = i;
         }
+        if (dp[maxInd][0] <= dp[i][0]){ maxInd = i; }
     }
     while (dp[maxInd][1] != INT_MIN)
     {
         ret.push_back(disks[maxInd]);
         maxInd = dp[maxInd][1];
     }
+    ret.push_back(disks[maxInd]);
+    
 
     sort(ret.begin(), ret.end(), check);
     return ret;
@@ -47,9 +53,9 @@ vector<vector<int>> diskStacking(vector<vector<int>> disks)
 
 int main()
 {
-    vector<vector<int>> disks = {{2,1,2},{3,2,3},{2,2,8},{2,3,4},{2,2,1},{4,4,5}};
+    vector<vector<int>> disks = {{2, 1, 2}, {3, 2, 5}, {2, 3, 4}};
     vector<vector<int>> ret = diskStacking(disks);
-
+    cout<<"\n";
     for (int i = 0; i < ret.size(); i++)
     {
         cout << ret[i][0] << " " << ret[i][1] << " " << ret[i][2] << "\n";
